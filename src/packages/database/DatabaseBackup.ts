50d52811ac0bfbdac3e4e1d7b8f36dd8e6ab10bc
Heryson1616 <dufon45@gmail.com>
2022-01-17 10:16:09 -0300

    typescript compiling with an problem BECAUSE I FORGOT A ;

--- src/packages/database/DatabaseBackup.ts
+++ src/packages/database/DatabaseBackup.ts
@@ -8,18 +8,18 @@ module.exports = (ket: Client) => {
         if (!db) return setTimeout(() => module.exports(ket), 5_000)
         Object.entries(db).forEach(async ([key, value]) => typeof value === 'object'
             ? ket.createMessage(ket.config.channels.database, `Backup da table \`${key}\``, { name: `${key}.json`, file: JSON.stringify((await db[key].getAll())) })
-            : null)
+            : null);
 
         //  cache controller
         let users = (await global.session.postgres.query(`SELECT id from users;`)).rows.map(u => u.id),
             nonCached = [];
         ket.users.forEach((u) => !users.includes(u.id) && u.id !== ket.user.id ? ket.users.delete(u.id) : null);
         users.forEach(user => !ket.users.has(user) ? nonCached.push(user) : null);
-        for (let i in nonCached) new Promise((res, rej) => setTimeout(async () => res(await ket.getRESTUser(nonCached[i])), 5_000))
+        for (let i in nonCached) new Promise((res, rej) => setTimeout(async () => res(await ket.getRESTUser(nonCached[i])), 5_000));
 
         //  checando banimentos
         (await db.blacklist.getAll()).forEach(async user => !(await db.users.find(user.id)).banned ? db.users.update(user.id, { banned: true }) : null);
     }
-    backupAndCacheController()
-    setInterval(() => backupAndCacheController(), 60_000 * 30)
+    backupAndCacheController();
+    setInterval(() => backupAndCacheController(), 60_000 * 30);
 }
