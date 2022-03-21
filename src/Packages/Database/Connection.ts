a0ea39fb4bca5902832b809b3d96c1be80e717f1
Heryson1616 <dufon45@gmail.com>
2022-03-21 00:52:35 -0300

    just to deploy app on heroku

--- src/Packages/Database/Connection.ts
+++ src/Packages/Database/Connection.ts
@@ -11,9 +11,17 @@ export default async () => {
         port: Number(process.env.PORT),
         ssl: process.env.SSL === 'false' ? false : { rejectUnauthorized: false }
     });
-    console.log(process.env.SSL === 'false' ? false : { rejectUnauthorized: false })
+    console.log({
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+        user: process.env.USER,
+        host: process.env.HOST,
+        port: Number(process.env.PORT),
+        ssl: process.env.SSL === 'false' ? false : { rejectUnauthorized: false }
+    })
+
     await postgres.connect()
-        .then(() => {
+        .then(async () => {
             postgres.users = new table<userSchema>('users', 'id', postgres);
             postgres.servers = new table<serverSchema>('servers', 'id', postgres);
             postgres.commands = new table<commandSchema>('commands', 'name', postgres);
@@ -21,71 +29,74 @@ export default async () => {
             postgres.blacklist = new table<blacklistSchema>('blacklist', 'id', postgres);
             postgres.tables = ['users', 'servers', 'commands', 'globalchat', 'blacklist'];
 
+            await verifyTables(postgres);
             console.log('DATABASE', '√ Banco de dados operante ', 32);
         })
         .catch((error) => console.log('DATABASE', `x Não foi possível realizar conexão ao banco de dados: ${error}`, 41))
 
+    return postgres;
+}
+
+async function verifyTables(postgres: Client) {
     /* DATABASE TESTS */
     await postgres.query(`SELECT id FROM public.users;`)
         .catch(async () => {
             console.log('DATABASE', 'Criando table users', 2);
             await postgres.query(`
-            CREATE TABLE "users" (
-                "id" VARCHAR(20) NOT NULL PRIMARY KEY,
-                "prefix" VARCHAR(3),
-                "lang" VARCHAR(2),
-                "commands" INTEGER NOT NULL,
-                "banned" TEXT            
-            );`)
+        CREATE TABLE "users" (
+            "id" VARCHAR(20) NOT NULL PRIMARY KEY,
+            "prefix" VARCHAR(3),
+            "lang" VARCHAR(2),
+            "commands" INTEGER NOT NULL,
+            "banned" TEXT            
+        );`)
         })
 
     await postgres.query(`SELECT id FROM public.servers;`)
         .catch(async () => {
             console.log('DATABASE', 'Criando table servers', 2);
             await postgres.query(`
-            CREATE TABLE "servers" (
-                "id" VARCHAR(20) NOT NULL PRIMARY KEY,
-                "lang" VARCHAR(2),
-                "globalchat" VARCHAR(20),
-                "partner" BOOLEAN,
-                "banned" TEXT
-            );`)
+        CREATE TABLE "servers" (
+            "id" VARCHAR(20) NOT NULL PRIMARY KEY,
+            "lang" VARCHAR(2),
+            "globalchat" VARCHAR(20),
+            "partner" BOOLEAN,
+            "banned" TEXT
+        );`)
         })
 
     await postgres.query(`SELECT name FROM public.commands;`)
         .catch(async () => {
             console.log('DATABASE', 'Criando table commands', 2);
             await postgres.query(`
-            CREATE TABLE "commands" (
-                "name" TEXT NOT NULL PRIMARY KEY,
-                "maintenance" BOOLEAN,
-                "reason" TEXT
-            );`)
+        CREATE TABLE "commands" (
+            "name" TEXT NOT NULL PRIMARY KEY,
+            "maintenance" BOOLEAN,
+            "reason" TEXT
+        );`)
         })
 
     await postgres.query(`SELECT id FROM public.globalchat;`)
         .catch(async () => {
             console.log('DATABASE', 'Criando table globalchat', 2);
             await postgres.query(`
-            CREATE TABLE "globalchat" (
-                "id" VARCHAR(20) NOT NULL PRIMARY KEY,
-                "guild" VARCHAR(20) NOT NULL,
-                "author" VARCHAR(20) NOT NULL,
-                "editCount" INTEGER NOT NULL DEFAULT 0,
-                "messages" VARCHAR(40)[]
-            );`)
+        CREATE TABLE "globalchat" (
+            "id" VARCHAR(20) NOT NULL PRIMARY KEY,
+            "guild" VARCHAR(20) NOT NULL,
+            "author" VARCHAR(20) NOT NULL,
+            "editCount" INTEGER NOT NULL DEFAULT 0,
+            "messages" VARCHAR(40)[]
+        );`)
         })
 
     await postgres.query(`SELECT id FROM public.blacklist;`)
         .catch(async () => {
             console.log('DATABASE', 'Criando table blacklist', 2);
             await postgres.query(`
-            CREATE TABLE "blacklist" (
-                "id" VARCHAR(20) NOT NULL PRIMARY KEY,
-                "timeout" INTEGER NOT NULL,
-                "warns" INTEGER NOT NULL DEFAULT 1
-            );`)
+        CREATE TABLE "blacklist" (
+            "id" VARCHAR(20) NOT NULL PRIMARY KEY,
+            "timeout" INTEGER NOT NULL,
+            "warns" INTEGER NOT NULL DEFAULT 1
+        );`)
         })
-
-    return postgres;
 }
